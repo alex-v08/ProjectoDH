d3afff50a4e1a5b07f3f09b6a2711c0c1c39f6a7
Alex Velasquez <alex.velasquez08@outlook.com>
2023-08-01 16:37:38 -0300

    AttributeExceptions

--- Backend/crudproduct/crud/src/main/java/Global/exceptions/AttributeException.java
+++ Backend/crudproduct/crud/src/main/java/Global/exceptions/AttributeException.java
@@ -1,2 +1,15 @@
-package Global.exceptions;public class AttributeException {
+package Global.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AttributeException extends Exception{
+
+    public AttributeException(String message) {
+        super(message);
+    }
+
+
 }
