eb6d72ddfa278027eb8226bc7cdde6c5e9e355ca
Alex Velasquez <alex.velasquez08@outlook.com>
2023-08-15 12:27:53 -0300

    constructor sin id agregado

--- Backend/crud-user/src/main/java/com/oceanwinds/crud_user/entity/User.java
+++ Backend/crud-user/src/main/java/com/oceanwinds/crud_user/entity/User.java
@@ -10,11 +10,9 @@ import lombok.*;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-
-
-
+@ToString
 @Getter
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -44,7 +42,15 @@ public class User {
     @Setter
     private UserEnum role;
 
-
-
+    public User(String name, String lastName, String email, String dni, String password, String phone, String address, UserEnum role) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.dni = dni;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+    }
 }
 
