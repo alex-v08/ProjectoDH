85fe93b1ec1abdaa38a349de316674bf37dbd1c0
Alex Velasquez <alex.velasquez08@outlook.com>
2023-08-15 09:33:03 -0300

    crud user

---
+++ Backend/crud-user/src/test/java/com/oceanwinds/crud_user/CrudUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceanwinds.crud_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
