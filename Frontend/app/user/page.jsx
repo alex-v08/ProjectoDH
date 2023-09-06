f8a151f8e9b48da96a7efb784fd4f90090a194b3
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-09-06 19:16:35 -0300

    incorpore el fech de user

--- Frontend/app/user/page.jsx
+++ Frontend/app/user/page.jsx
@@ -1,23 +1,84 @@
 'use client'
 
+import Image from 'next/image'
 import HeroUser from '@/components/user/HeroUser'
-import { useState } from 'react'
+import { useAuth } from '@/context/authContext'
+import { useState, useEffect } from 'react'
+import { BsCameraFill } from 'react-icons/bs'
 
 export default function User() {
   const [userForm, setUserForm] = useState({
+    address: '',
+    dni: '',
     email: '',
-    password: ''
+    lastName: '',
+    name: '',
+    phone: '',
+    active: ''
   })
   const [error, setError] = useState('')
+  const { user, loading } = useAuth()
+  const [userInfo, setUserInfo] = useState(null)
+  const [send, setSend] = useState(false)
+
+  useEffect(() => {
+    // Realiza una solicitud HTTP para obtener los datos del usuario y su rol.
+    const fetchUserInfo = async () => {
+      try {
+        const hostUrl = process.env.NEXT_PUBLIC_HOST_URL
+        const response = await fetch(
+          `${hostUrl}/users/list/{uuid}?uuid=${user.uid}`
+        )
+        if (response.ok) {
+          const userData = await response.json()
+          const userFormData = {
+            address: userData[0].address || '', // Actualiza los campos según la estructura de userData
+            dni: userData[0].dni || '',
+            email: userData[0].email || '',
+            lastName: userData[0].lastName || '',
+            name: userData[0].name || '',
+            phone: userData[0].phone || '',
+            active: userData[0].active || '',
+            role: userData[0].role || '',
+            uuid: userData[0].uuid || ''
+          }
+          console.log(userFormData)
+          setUserForm(userFormData)
+          setUserInfo(userData[0])
+        } else {
+        }
+      } catch (error) {
+        console.error(error.message)
+      }
+    }
+
+    if (user) {
+      fetchUserInfo()
+    }
+  }, [user, send])
 
   const handleSubmit = async e => {
     e.preventDefault()
     setError('')
     try {
-      await login(userForm.email, userForm.password)
-      router.push('/')
+      const hostUrl = process.env.NEXT_PUBLIC_HOST_URL
+      console.log(userForm)
+      const response = await fetch(`${hostUrl}/users/${userInfo.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userForm) // Send the updated user data as JSON
+      })
+
+      if (response.ok) {
+        console.log('User data updated successfully')
+        setSend(!send)
+      } else {
+        console.error('Error updating user data')
+      }
     } catch (error) {
-      setError(errorMessages[error.code] || error.message)
+      console.error(error.message)
     }
   }
 
@@ -30,8 +91,57 @@ export default function User() {
       <div className=' bg-[#f2f5fa]'>
         <div className='container'>
           <div className='relative -top-20 left-0 z-10 flex w-full flex-col  gap-5 rounded-lg  lg:flex-row '>
-            <div className='w-full max-w-[506px] rounded-lg bg-white px-5 py-5 drop-shadow-md lg:px-10 lg:py-9 xl:px-7'>
-              1
+            <div className='flex w-full flex-col items-center rounded-lg bg-white px-5 py-5 drop-shadow-md lg:max-w-[506px] lg:px-10 lg:py-9 xl:px-7'>
+              {user && userInfo ? (
+                <>
+                  <div className='photo relative'>
+                    {user.photoURL ? (
+                      <Image
+                        src={user.photoURL}
+                        alt='Picture of the user'
+                        width={128}
+                        height={128}
+                        className='rounded-full border-2 border-sky-500'
+                      />
+                    ) : (
+                      <div className='flex h-[128px] w-[128px] cursor-pointer items-center justify-center rounded-full border-2 border-sky-500 bg-gradient-to-r from-cyan-500 to-blue-500'>
+                        <div className='font-bold uppercase text-white'>
+                          {user.email[0]}
+                        </div>
+                      </div>
+                    )}
+                    <BsCameraFill className='absolute -right-2 bottom-0 h-[40px] w-[40px] cursor-pointer rounded-full bg-sky-500 p-2 text-2xl text-white' />
+                  </div>
+                  <div className='mt-4 text-2xl font-bold text-sky-950'>
+                    {user.displayName}
+                  </div>
+                  <div className='mt-2 rounded-lg border border-emerald-500 px-3 py-1 lowercase text-emerald-500'>
+                    {userInfo.role}
+                  </div>
+                  <div className='mt-4 flex w-full flex-col gap-3'>
+                    <div className='border-b border-t pb-3 pt-3'>
+                      <span className='pl-2 font-semibold'>Correo:</span>{' '}
+                      {userInfo.email}
+                    </div>
+                    <div className='border-b pb-3'>
+                      <span className='pl-2 font-semibold'>DNI:</span>{' '}
+                      {userInfo.dni}
+                    </div>
+                    <div className='border-b pb-3'>
+                      <span className='pl-2 font-semibold'>Teléfono:</span>{' '}
+                      {userInfo.phone}
+                    </div>
+                    <div className='border-b pb-3'>
+                      <span className='pl-2 font-semibold'>Direccion:</span>{' '}
+                      {userInfo.address}
+                    </div>
+                    <div className='border-b pb-3'>
+                      <span className='pl-2 font-semibold'>Estado:</span>{' '}
+                      {userInfo.active ? 'activo' : ''}
+                    </div>
+                  </div>
+                </>
+              ) : null}
             </div>
             <div className='w-full rounded-lg bg-white px-5 py-5 drop-shadow-md lg:px-10 lg:py-9 xl:px-7'>
               <form onSubmit={handleSubmit}>
@@ -53,6 +163,7 @@ export default function User() {
                       onChange={handleChange}
                       className='focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none'
                       placeholder='Nombre'
+                      value={userForm.name}
                     />
                   </div>
                   <div>
@@ -69,6 +180,7 @@ export default function User() {
                       onChange={handleChange}
                       className='focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none'
                       placeholder='Apellido'
+                      value={userForm.lastName}
                     />
                   </div>
                   <div>
@@ -85,6 +197,7 @@ export default function User() {
                       onChange={handleChange}
                       className='focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none'
                       placeholder='ejemplo@email.com'
+                      value={userForm.email}
                     />
                   </div>
                   <div>
@@ -101,6 +214,7 @@ export default function User() {
                       onChange={handleChange}
                       className='focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none'
                       placeholder='DNI'
+                      value={userForm.dni}
                     />
                   </div>
                   <div>
@@ -117,6 +231,7 @@ export default function User() {
                       onChange={handleChange}
                       className='focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none'
                       placeholder='Teléfono'
+                      value={userForm.phone}
                     />
                   </div>
                   <div>
@@ -133,6 +248,7 @@ export default function User() {
                       onChange={handleChange}
                       className='focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none'
                       placeholder='Dirección'
+                      value={userForm.address}
                     />
                   </div>
                 </div>
