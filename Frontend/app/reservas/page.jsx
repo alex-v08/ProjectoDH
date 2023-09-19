51e354ba605c3771453913607d6d5f444aa0f695
fabri <fabriciodanielturrina@gmail.com>
2023-09-19 11:15:49 -0300

    working in booking  history section

--- Frontend/app/reservas/page.jsx
+++ Frontend/app/reservas/page.jsx
@@ -60,17 +60,33 @@ const HistorialReservas = () => {
     }
   }
 
-  console.log(data)
+  function calculateDays(startDate, endDate) {
+    // Parse las fechas en objetos Date
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+  
+    // Calcula la diferencia en milisegundos
+    const timeDifference = end - start;
+  
+    // Convierte la diferencia en días
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  
+    return daysDifference;
+  }
+
+  
   return (
     <div className='bg-[#f2f5fa]'>
       <div className='container mx-auto min-h-screen p-4 pt-8'>
         <SectionTitle antetitulo='volve a disfrutar' titulo='Tus reservas' />
-        <div className='grid grid-cols-1 justify-items-center gap-x-6 gap-y-3 sm:grid-cols-2'>
+        <div className='grid grid-cols-1  gap-x-6 gap-y-6 sm:grid-cols-2'>
           {userId ? (
             data.map(booking => (
+              <a href={`/detail/${booking.product.id}`}>
+
               <div
                 key={booking.id}
-                className='group flex h-full w-full justify-start rounded shadow-sm transition-transform duration-300 ease-in-out hover:scale-[1.02] '
+                className='group flex h-full w-full justify-start rounded shadow-lg transition-transform duration-300 ease-in-out hover:scale-[1.02] '
               >
                 <div className='relative flex h-36 min-h-[150px] w-full flex-col overflow-hidden lg:h-[200px] lg:max-w-[35%] xl:max-w-[35%]'>
                   <img
@@ -80,23 +96,39 @@ const HistorialReservas = () => {
                   />
                 </div>
                 <div className='flex w-full flex-col flex-wrap pl-4  transition duration-300 ease-in-out group-hover:bg-white'>
-                  <p>Nombre del Producto: {booking.product.name}</p>
-                  <p>
-                    Fecha de Reserva:{' '}
-                    {dayjs(booking.dateCreated).format('DD-MM-YYYY')}
+                  <p className='truncate pb-3 text-xl font-bold uppercase text-sky-900'>
+                    {booking.product.name}
                   </p>
-                  <p>
-                    Fecha de Inicio:{' '}
-                    {dayjs(booking.dateInit).format('DD-MM-YYYY')}
+                  <p className='font-small pb-3 text-sm text-gray-500'>
+                    Hiciste tu reserva el día:{' '}
+                    <span className='mr-2 text-xs font-semibold uppercase text-gray-500'>
+                      {' '}
+                      {dayjs(booking.dateCreated).format('DD-MM-YYYY')}
+                    </span>
                   </p>
-                  <p>
-                    Fecha de Fin: {dayjs(booking.dateEnd).format('DD-MM-YYYY')}
+                  <p className='font-small pb-3 text-sm text-gray-500'>
+                    Fecha de ingreso:{' '}
+                    <span className='mr-2 text-xs font-semibold uppercase text-gray-500'>
+                      {dayjs(booking.dateInit).format('DD-MM-YYYY')}
+                    </span>
+                  </p>
+                  <p className='font-small pb-3 text-sm text-gray-500'>
+                    Fecha de salida:{' '}
+                    <span className='mr-2 text-xs font-semibold uppercase text-gray-500'>
+                      {dayjs(booking.dateEnd).format('DD-MM-YYYY')}
+                    </span>
+                  </p>
+                  <p className='font-small pb-3 text-sm text-gray-500'>
+                    Cantidad de días:{' '}
+                    <span className='mr-2 text-xs font-semibold uppercase text-gray-500'>
+                      {calculateDays(booking.dateInit, booking.dateEnd)}
+                    </span>
                   </p>
                 </div>
               </div>
-            ))
+              </a>   ))
           ) : (
-            <div className='font-bold text-sky-900'>
+            <div className='text-center font-bold text-sky-900'>
               Cargando historial de reservas...
             </div>
           )}
