6b882c6c53ff34780ee030bd6d519f95b8ca8797
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-09-19 09:40:40 -0300

    skeleton y opt de imagen en favoritos

--- Frontend/app/favoritos/page.jsx
+++ Frontend/app/favoritos/page.jsx
@@ -3,12 +3,16 @@ import { useState, useEffect } from 'react'
 import { useAuth } from '@/context/authContext'
 import SectionTitle from '@/components/SectionTitle'
 import { BsFillTrashFill } from 'react-icons/bs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { staticBlurDataUrl } from '@/components/util/staticBlurDataUrl'
 
 const CurrentFavs = () => {
   const [data, setData] = useState([])
   const [changeData, setChangeData] = useState(true)
   const [userId, setUserId] = useState(null)
   const [isRemove, setIsRemove] = useState(false)
+  const [isLoadingData, setIsLoadingData] = useState(true)
 
   const { user } = useAuth()
 
@@ -20,11 +24,10 @@ const CurrentFavs = () => {
         const response = await fetch(
           `${hostUrl}/users/list/{uuid}?uuid=${user.uid}`
         )
-        console.log('FETCH USER')
+        // console.log('FETCH USER')
         if (response.ok) {
           const userData = await response.json()
           setUserId(userData[0].id)
-          fetchData(userId)
         } else {
           // Maneja los errores de la solicitud aquí.
         }
@@ -38,30 +41,33 @@ const CurrentFavs = () => {
     }
   }, [user, userId])
 
+  // Utiliza un useEffect para cargar los datos cuando userId cambia
   useEffect(() => {
-    fetchData(userId)
-  }, [isRemove])
-
-  async function fetchData(userId) {
-    try {
-      console.log('FETCH DATA')
-      console.log(userId)
-      const response = await fetch(
-        `${hostUrl}/api/favorites/findByUserId/${userId}`
-      )
-      if (!response.ok) {
-        throw new Error(
-          'Error al intentar cargar todos los registros: . Response: ' +
-            response.status
-        )
+    if (userId !== null) {
+      async function fetchData() {
+        try {
+          // console.log('FETCH DATA')
+          // console.log(userId)
+          const response = await fetch(
+            `${hostUrl}/api/favorites/findByUserId/${userId}`
+          )
+          if (!response.ok) {
+            throw new Error(
+              'Error al intentar cargar todos los registros: . Response: ' +
+                response.status
+            )
+          }
+          const jsonData = await response.json()
+          setData(jsonData)
+          setIsLoadingData(false)
+        } catch (error) {
+          console.error('Error cargando los registros: ', error)
+        }
       }
-      const jsonData = await response.json()
-      setData(jsonData)
-      console.log(data)
-    } catch (error) {
-      console.error('Error cargando los registros: ', error)
+
+      fetchData() // Llamar a fetchData aquí cuando userId no sea nulo
     }
-  }
+  }, [userId, isRemove])
 
   useEffect(() => {
     if (changeData) {
@@ -99,53 +105,93 @@ const CurrentFavs = () => {
     }
   }
 
+  const skeletonCards = Array.from({ length: 8 }).map((_, index) => (
+    <div
+      key={index}
+      className='group relative flex w-full max-w-[450px] flex-col overflow-hidden rounded-lg border shadow-md transition-transform duration-300 ease-in-out hover:scale-[1.02] lg:max-w-full lg:flex-row'
+    >
+      <div className='relative h-36 min-h-[150px] w-full animate-pulse overflow-hidden bg-gray-300 lg:h-[200px] lg:max-w-[35%] xl:max-w-[35%]'></div>
+      <div className='w-full transition duration-300 ease-in-out group-hover:bg-white'>
+        <div className='flex flex-col px-5 py-6 text-xs sm:px-7'>
+          <div className='flex justify-between'>
+            <div className='skeleton-category w-10 animate-pulse bg-gray-300'></div>
+            <div className='flex'>
+              <div className='skeleton-star h-4 w-5 animate-pulse bg-gray-300'></div>
+              <div className='skeleton-rating h-4 w-16 animate-pulse bg-gray-300'></div>
+            </div>
+          </div>
+          <div className='skeleton-name mt-2 h-5 animate-pulse bg-gray-300'></div>
+          <div className='skeleton-description mt-2 h-10 animate-pulse bg-gray-300'></div>
+        </div>
+        <div className='flex items-center justify-between px-7 py-3'>
+          <div className='skeleton-price h-6 w-24 animate-pulse bg-gray-300'></div>
+          <div className='skeleton-button h-9 w-32 animate-pulse rounded-lg bg-gray-300'></div>
+        </div>
+      </div>
+    </div>
+  ))
+
   return (
     <div className='bg-[#f2f5fa]'>
-      <div className='container mx-auto min-h-screen p-4 pt-8'>
+      <div className='container mx-auto min-h-screen pb-10 pt-8'>
         <SectionTitle antetitulo='elegidos' titulo='Tus favoritos' />
         <div className='grid grid-cols-1 justify-items-center gap-x-6 gap-y-10 sm:grid-cols-2 xl:gap-x-8'>
-          {data.map(item => (
-            <a
-              key={item.id}
-              className='group relative flex w-full max-w-[450px] flex-col overflow-hidden rounded-lg border shadow-md transition-transform duration-300 ease-in-out hover:scale-[1.02] lg:max-w-full lg:flex-row'
-              href={`/detail/${item.product.id}`}
-            >
-              <div className='relative h-36 min-h-[150px] w-full overflow-hidden lg:h-[200px] lg:max-w-[35%] xl:max-w-[35%]'>
-                <img
-                  src={`${item.product.imageUrl}1.png`}
-                  alt='imagen de la embarcacion'
-                  className='h-full w-full object-cover'
-                />
-              </div>
-              <div className='flex h-full w-full flex-col justify-between px-4 py-4 transition duration-300 ease-in-out group-hover:bg-white lg:px-8'>
-                <div>
-                  <h3 className='truncate pb-2 text-xl font-bold uppercase text-sky-900'>
-                    {item.product.name}
-                  </h3>
-                  <div className='font-small mb-4 line-clamp-3 text-sm text-gray-500'>
-                    {item.product.description}
+          {isLoadingData ? (
+            <>{skeletonCards}</>
+          ) : data.length === 0 ? (
+            <p>No tienes favoritos todavía</p>
+          ) : (
+            <>
+              {data.map(item => (
+                <Link
+                  key={item.id}
+                  className='group relative flex w-full max-w-[450px] flex-col overflow-hidden rounded-lg border shadow-md transition-transform duration-300 ease-in-out hover:scale-[1.02] lg:max-w-full lg:flex-row'
+                  href={`/detail/${item.product.id}`}
+                >
+                  <div className='relative h-36 min-h-[150px] w-full overflow-hidden lg:h-[200px] lg:max-w-[35%] xl:max-w-[35%]'>
+                    <Image
+                      src={`${item.product.imageUrl}1.png`}
+                      alt='imagen de la embarcacion'
+                      loading='eager'
+                      fill
+                      blurDataURL={staticBlurDataUrl()}
+                      placeholder='blur'
+                      sizes='(max-width: 768px) 100vw'
+                      style={{ objectFit: 'cover' }}
+                      className='transition duration-150 ease-in-out group-hover:brightness-105'
+                    />
                   </div>
-                </div>
-                <div className='flex items-center justify-between'>
-                  <h3 className='text-l flex flex-col font-extrabold leading-none text-sky-500 md:flex-row'>
-                    <span className='mr-2 text-xs font-semibold uppercase text-gray-500'>
-                      Por noche
-                    </span>
-                    <span>$ {item.product.pricePerDay}</span>
-                  </h3>
-                  <a
-                    className=' cursor-pointer rounded px-2 py-1 text-sm text-rose-400 transition hover:rotate-12 hover:text-rose-500'
-                    onClick={e => {
-                      e.preventDefault()
-                      handleRemoveFav(item.product.id)
-                    }}
-                  >
-                    <BsFillTrashFill className='h-6 w-6' />
-                  </a>
-                </div>
-              </div>
-            </a>
-          ))}
+                  <div className='flex h-full w-full flex-col justify-between px-4 py-4 transition duration-300 ease-in-out group-hover:bg-white lg:px-8'>
+                    <div>
+                      <h3 className='truncate pb-2 text-xl font-bold uppercase text-sky-900'>
+                        {item.product.name}
+                      </h3>
+                      <div className='font-small mb-4 line-clamp-3 text-sm text-gray-500'>
+                        {item.product.description}
+                      </div>
+                    </div>
+                    <div className='flex items-center justify-between'>
+                      <h3 className='text-l flex flex-col font-extrabold leading-none text-sky-500 md:flex-row'>
+                        <span className='mr-2 text-xs font-semibold uppercase text-gray-500'>
+                          Por noche
+                        </span>
+                        <span>$ {item.product.pricePerDay}</span>
+                      </h3>
+                      <button
+                        className=' cursor-pointer rounded px-2 py-1 text-sm text-rose-400 transition hover:rotate-12 hover:text-rose-500'
+                        onClick={e => {
+                          e.preventDefault()
+                          handleRemoveFav(item.product.id)
+                        }}
+                      >
+                        <BsFillTrashFill className='h-6 w-6' />
+                      </button>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </>
+          )}
         </div>
       </div>
     </div>
